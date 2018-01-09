ec2d1ac8ceedb2e75ef3eac36a8069c5972b0637
Jerel Miller <jerelmiller@gmail.com>
2018-01-09 15:38:51 -0800

    Add basic functionality to search users

--- src/screens/users.js
+++ src/screens/users.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SearchFilter from '../components/SearchFilter'
 import SearchList from '../components/SearchList'
 import { Route } from 'react-router-dom'
@@ -6,21 +6,50 @@ import './users.css'
 
 import User from './users/user'
 
-const Users = ({ match: { path } }) => (
-  <main>
-    <div className="search">
-      <div className="list-header">
-        <SearchFilter />
-      </div>
-      <div className="list">
-        <SearchList users={[]} />
-      </div>
-    </div>
+class Users extends Component {
+  state = {
+    query: 'FocusVision',
+    users: []
+  }
 
-    <div className="content">
-      <Route path={`${path}/:userId`} component={User} />
-    </div>
-  </main>
-)
+  componentDidMount() {
+    const { query } = this.state
+
+    fetch(`https://api.github.com/search/users?q=${query}`, {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `Basic ${btoa(
+          `${process.env.REACT_APP_GITHUB_USERNAME}:${
+            process.env.REACT_APP_GITHUB_TOKEN
+          }`
+        )}`
+      }
+    })
+      .then(response => response.json())
+      .then(({ items }) => this.setState({ users: items }))
+  }
+
+  render() {
+    const { match: { path } } = this.props
+    const { users, query } = this.state
+
+    return (
+      <main>
+        <div className="search">
+          <div className="list-header">
+            <SearchFilter query={query} />
+          </div>
+          <div className="list">
+            <SearchList users={users} />
+          </div>
+        </div>
+
+        <div className="content">
+          <Route path={`${path}/:userId`} component={User} />
+        </div>
+      </main>
+    )
+  }
+}
 
 export default Users
