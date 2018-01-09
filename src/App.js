08f2813807e5eb0608547f1979b4dee484b4350e
Jerel Miller <jerelmiller@gmail.com>
2018-01-09 13:16:02 -0800

    Start working on users screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
+import Users from './screens/users'
+
 const App = () => (
   <Switch>
     <Route path="/" exact render={() => <Redirect to="/users" />} />
+    <Route path="/users" component={Users} />
   </Switch>
 )
 
---
+++ src/screens/users.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './users.css'
+
+const Users = () => (
+  <main>
+    <div className="search">
+      <div className="list-header" />
+      <div className="list" />
+    </div>
+
+    <div className="content" />
+  </main>
+)
+
+export default Users
