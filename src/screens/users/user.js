bef0dbb9faccdfb4800316424717a73d4bc1f2dc
Jerel Miller <jerelmiller@gmail.com>
2018-01-09 16:27:18 -0800

    Fetch user and repos on show page

--- src/screens/users/user.js
+++ src/screens/users/user.js
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import request from '../../utils/api'
 
-const User = () => <div>User</div>
+class User extends Component {
+  state = {
+    loading: true,
+    user: {},
+    repos: []
+  }
+
+  componentDidMount() {
+    const { match: { params: { login } } } = this.props
+
+    Promise.all([
+      request(`/users/${login}`),
+      request(`/users/${login}/repos`)
+    ]).then(([user, repos]) =>
+      this.setState({
+        user,
+        repos,
+        loading: false
+      })
+    )
+  }
+
+  render() {
+    const { loading, user, repos } = this.state
+
+    if (loading) {
+      return null
+    }
+
+    return (
+      <Fragment>
+        <div class="user-details">
+          <img src={user.avatar_url} class="user-avatar" />
+          <div className="user-details-meta">
+            <h3>{user.login}</h3>
+            <p>{user.name}</p>
+          </div>
+        </div>
+        <h3>Repos</h3>
+        <div class="list list-repo">
+          {repos.map(repo => (
+            <a href={repo.html_url} target="_blank" class="list-item">
+              <strong>{repo.name}</strong>
+              {repo.description}
+            </a>
+          ))}
+        </div>
+      </Fragment>
+    )
+  }
+}
 
 export default User
